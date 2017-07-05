61bdec4728f308800e04546aab7711738865e89c
Russell Osborne <russell@theironyard.com>
2017-07-05 09:38:29 -0400

    Adding html + javascript lecture

---
+++ 02/3-wednesday-8/main.js
@@ -0,0 +1,13 @@
+console.log("The Best Dog Website: We are hiring good dogs");
+
+const dog = "Scarlet";
+
+const cat = dog;
+
+// console.log(cat);
+// console.warn(cat);
+// console.error(cat);
+// console.info(cat);
+
+
+console.log( 8 * 12 / 1.23 );

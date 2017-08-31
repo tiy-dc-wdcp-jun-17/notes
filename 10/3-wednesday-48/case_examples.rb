f287d0e0b9d30b40704cdeff263b19f59bec2d39
Russell Osborne <russell@theironyard.com>
2017-08-31 12:15:40 -0400

    Added tweaked notes from wednesday

---
+++ 10/3-wednesday-48/case_examples.rb
@@ -0,0 +1,4 @@
+IAmTitleCase
+iAmCamelCase
+i_am_snake_case
+i_am_also_called_underscore
--- 10/3-wednesday-48/evented/db/seeds.rb
+++ 10/3-wednesday-48/evented/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+  Event.create location: FFaker::AddressUS.street_address, title: FFaker::Conference.name, open_at: FFaker::Time.datetime
+end
